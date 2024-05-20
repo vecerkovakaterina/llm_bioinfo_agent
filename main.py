68c9c2494597054e66f0f6673da85408c97f9f83
vecerkovakaterina <vecerkovakaterina1@gmail.com>
2024-05-20 12:59:07 +0200

    Parse function call

--- main.py
+++ main.py
@@ -19,6 +19,32 @@ def get_response_answer(ollama_api_generate_response):
     return ollama_api_generate_response.text
 
 
+ensembl_api = "https://rest.ensembl.org"
+
+
+def look_up_species_by_ensembl_id(ensembl_id):
+    """Look up the species for any gene Ensembl ID e.g. ENSG00000157764"""
+    ext = f"/lookup/id/{ensembl_id}?"
+    request = requests.get(
+        ensembl_api + ext, headers={"Content-Type": "application/json"}
+    )
+    return request.json()["species"]
+
+
+def get_taxonomy_classification(species_name):
+    """Get taxonomy classification based on species name e.g. Homo sapiens"""
+    ext = f"/taxonomy/classification/{species_name}?"
+    request = requests.get(
+        ensembl_api + ext, headers={"Content-Type": "application/json"}
+    )
+    return request.json()
+
+
+tools_dict = {
+    "look_up_species_by_ensembl_id": look_up_species_by_ensembl_id,
+    "get_taxonomy_classification": get_taxonomy_classification,
+}
+
 tools = [
     {
         "name": "look_up_species_by_ensembl_id",
@@ -73,14 +99,14 @@ llm_call = get_response_answer(
                     "role": "user",
                     "content": "What species has the gene ENSMUSG00000017167?",
                 },
-                {
-                    "role": "assistant",
-                    "content": '{\n"tool": "look_up_species_by_ensembl_id",\n"tool_input": {\n"ensembl_id": "ENSMUSG00000017167"\n}\n}',
-                },
-                {
-                    "role": "user",
-                    "content": "Function call look_up_species_by_ensembl_id with query ENSMUSG00000017167 result is Mus musculus",
-                },
+                # {
+                #     "role": "assistant",
+                #     "content": '{\n"tool": "look_up_species_by_ensembl_id",\n"tool_input": {\n"ensembl_id": "ENSMUSG00000017167"\n}\n}',
+                # },
+                # {
+                #     "role": "user",
+                #     "content": "Function call look_up_species_by_ensembl_id with query ENSMUSG00000017167 result is Mus musculus",
+                # },
             ],
             "format": "json",
             "stream": False,
@@ -89,7 +115,33 @@ llm_call = get_response_answer(
     )
 )
 
-print(llm_call)
 
+def parse_response(json_llm_call):
+    if "response" not in json_llm_call:
+        return None
+    json_llm_call = json.loads(json_llm_call)
+    message_content = json_llm_call["message"]["content"]
+    nested_content = json.loads(message_content)
+    response = nested_content["tool_input"]["response"]
+    return response
+
+
+def parse_function_call(json_llm_call):
+    json_llm_call = json.loads(json_llm_call)
+    message_content = json_llm_call["message"]["content"]
+    nested_content = json.loads(message_content)
+    tool = nested_content["tool"]
+    tool_input = nested_content["tool_input"]
+    return tool, tool_input
+
+
+print(llm_call)
+print(parse_response(llm_call))
+function_call, function_args = parse_function_call(llm_call)
+
+print(tools_dict[function_call](**function_args))
+
+# TODO parse output for function calls
 # TODO logging
 # TODO gget functions as tools
+# TODO tools to a separate file
