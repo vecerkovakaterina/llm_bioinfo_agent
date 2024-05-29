5faf230bd452042efc9d26be0864cb2edf29498d
vecerkovakaterina <vecerkovakaterina1@gmail.com>
2024-05-29 14:38:54 +0200

    Clarify the prompts

--- tools.py
+++ tools.py
@@ -26,7 +26,11 @@ tools = [
             "properties": {
                 "ensembl_ids": {
                     "type": "list",
-                    "description": "Ensembl IDs of genes to search",
+                    "description": "Ensembl IDs of genes to search. Examples of Ensembl IDs are ENSMUSG00000017167, "
+                    "ENSG00000139618 or ENSDARG00000024771. An Ensembl stable ID consists of five "
+                    "parts: ENS(species)(object type)(identifier). (version). The second part is a "
+                    "three-letter species code. For human, there is no species code so IDs are in the "
+                    "form ENS(object type)(identifier).",
                 },
                 "ncbi": {
                     "type": "bool",
@@ -46,7 +50,8 @@ tools = [
     },
     {
         "name": "gget_search",
-        "description": "Fetch genes and transcripts from Ensembl using free-form search terms. Results are matched "
+        "description": "Fetch genes and transcripts from Ensembl using free-form search terms, for example gene name "
+        "like Brca2 or disease names like osteoarthrititis. Results are matched"
         "based on the 'gene name', 'description' and 'synonym' sections in the Ensembl database.",
         "parameters": {
             "type": "object",
@@ -56,11 +61,16 @@ tools = [
                     "description": "Free form search words (not case-sensitive) as a string or list of strings ("
                     "e.g.searchwords = ['GABA', 'gamma-aminobutyric']).",
                 },
+                "species": {
+                    "type": "string",
+                    "description": "Species or database to be searched. A species can be passed in the format "
+                    "'genus_species', e.g. 'homo_sapiens' or 'arabidopsis_thaliana'.",
+                },
                 "release": {
                     "type": "int",
                     "description": "Defines the Ensembl release number from which the files are fetched, e.g. 104. "
                     "This argument is overwritten if a specific database (which includes a release "
-                    "number) is passed to the species argument. Default: None -> latest Ensembl "
+                    "number) is passed to the species argument. Default: 111"
                     "release is used",
                 },
                 "id_type": {
@@ -91,22 +101,36 @@ You must always select one of the above tools and respond with only a JSON objec
   "tool": <name of the selected tool>,
   "tool_input": <parameters for the selected tool, matching the tool's JSON schema>
 }}
-"""
+
+If you don't have all required arguments to use a tool, use the get_further_clarification tool to get missing 
+information from the user."""
 
 
 # tools implementations
 
 
 def get_further_clarification(question):
-    """Ask user for missing information further clarification, instructions or more details"""
+    """Ask user for missing information further clarification, instructions or more details."""
     answer = input(question)
     return answer
 
 
 def gget_info(ensembl_ids, ncbi=True, uniprot=True, pdb=False):
-    """Fetch extensive gene and metadata from Ensembl, UniProt and NCBI using Ensembl IDs"""
+    """Fetch extensive gene and metadata from Ensembl, UniProt and NCBI using Ensembl IDs. Examples of Ensembl IDs
+    are ENSMUSG00000017167, ENSG00000139618 or ENSDARG00000024771. An Ensembl stable ID consists of five parts: ENS(
+    species)(object type)(identifier). (version). The second part is a three-letter species code. For human,
+    there is no species code so IDs are in the form ENS(object type)(identifier).
+    """
     if not ensembl_ids:
         return "Required argument ensembl_ids not provided!"
+
+    if isinstance(ensembl_ids, list):
+        for ensembl_id in ensembl_ids:
+            if not ensembl_id.startswith("ENS"):
+                return f"{ensembl_id} does not start with ENS and therefore is not an Ensembl ID!"
+    elif isinstance(ensembl_ids, str):
+        if not ensembl_ids.startswith("ENS"):
+            return f"{ensembl_ids} does not start with ENS and therefore is not an Ensembl ID!"
     result = gget.info(
         ens_ids=ensembl_ids, ncbi=ncbi, uniprot=uniprot, pdb=pdb, verbose=False
     )
@@ -115,14 +139,16 @@ def gget_info(ensembl_ids, ncbi=True, uniprot=True, pdb=False):
     return result
 
 
-def gget_search(search_words, release=None, id_type="gene", andor="or"):
-    """Fetch genes and transcripts from Ensembl using free-form search terms.
-    Results are matched based on the "gene name", "description" and "synonym" sections in the Ensembl database.
+def gget_search(search_words, species, release=111, id_type="gene", andor="or"):
+    """Fetch genes and transcripts from Ensembl using free-form search terms, for example gene names like Brca2 or
+    disease names like osteoarthrititis. Results are matched based on the "gene name", "description" and "synonym"
+    sections in the Ensembl database.
     """
     if not search_words:
         return "Required argument search_words not provided!"
     result = gget.search(
         searchwords=search_words,
+        species=species,
         release=release,
         id_type=id_type,
         andor=andor,
